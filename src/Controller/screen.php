1543ef6396f26453b0533988d1580304540931bf
lucasmapaiva <86642899+LucasMaPaiva@users.noreply.github.com>
2023-09-12 20:47:16 -0400

    pagina de adicionar criada

--- src/Controller/screen.php
+++ src/Controller/screen.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Task;
 
 class screen extends AbstractController
 {
     #[Route('/')]
     public function telaLogin(): Response
     {
+
         $pageTitle = 'Entrar';
         return $this->render('login.html.twig', [
             'pageTitle' => $pageTitle,
@@ -18,11 +21,16 @@ class screen extends AbstractController
     }
 
     #[Route('/pagina-inicial')]
-    public function homePage(): Response
+    public function homePage(EntityManagerInterface $entityManager): Response
     {
+
+        $taskRepository = $entityManager->getRepository(Task::class);
+        $todos = $taskRepository->findAll();
+
         $pageTitle = 'Lista de tarefas';
         return $this->render('home.html.twig', [
             'pageTitle' => $pageTitle,
+                'todos' => $todos
         ]);
     }
 
@@ -33,5 +41,19 @@ class screen extends AbstractController
         return $this->render('register.html.twig', [
             'pageTitle' => $pageTitle,
         ]);
+    }
+
+    #[Route('/criar-lista')]
+    public function criar(): Response
+    {
+
+        $pageTitle = 'Criar lista';
+        return $this->render('create.html.twig', [
+            'pageTitle' => $pageTitle,
+        ]);
+    }
+
+    private function getDoctrine()
+    {
     }
 }
